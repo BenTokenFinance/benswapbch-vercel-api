a8002a5d750cd7a147e2e85e9009436762e23413
Ben Token Finance <admin@bentoken.finance>
2021-10-13 15:41:57 -0700

    Update others.ts

--- utils/others.ts
+++ utils/others.ts
@@ -2,21 +2,19 @@ const https = require('https');
 
 const url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin-cash&vs_currencies=usd";
 
-
-
 export const getBchPrice =  async () => {
     let body = null;
-    
+
     await https.get(url, (res:any) => {
-        let body = "";
+        let data = "";
     
         res.on("data", (chunk:any) => {
-            body += chunk;
+            data += chunk;
         });
     
         res.on("end", () => {
             try {
-                body = JSON.parse(body);
+                body = JSON.parse(data);
                 // do something with JSON
             } catch (error) {
                 // do something?
