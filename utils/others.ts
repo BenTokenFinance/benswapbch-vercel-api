133bdd628d4b1290208dbbc8161b4042656d01ef
Ben Token Finance <admin@bentoken.finance>
2021-10-13 14:56:40 -0700

    Update others.ts

--- utils/others.ts
+++ utils/others.ts
@@ -1,4 +1,4 @@
-import { fetch } from 'node-fetch';
+import fetch from 'node-fetch';
 
 export const getBchPrice =  async () => {
     const response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin-cash&vs_currencies=usd");
